3b3f84c54b0a75322b9d7641b1b8a2ae455c11f0
Jonathan Swinney <jswinney@amazon.com>
2025-07-17 22:07:32 +0000

    clean up the python virtual env setup

--- test/container-script.sh
+++ test/container-script.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 set -e
-
-source .test/bin/activate
 cd /io
-pip3 install --progress-bar off --upgrade pip
 
 # Check if we will have a mismatch between latest release and wheel, by doing a dry-run
 pip3 install --dry-run --progress-bar off --report pip_latest.json $PIP_EXTRA_ARGS $PACKAGE_LIST &> dryrun_output.log
-
 pip3 install --prefer-binary --progress-bar off --report pip_binary.json $PIP_EXTRA_ARGS $PACKAGE_LIST
+
 python3 test-script.py
